d0fd48db1a1b0f6e294ddab4c1261206767b58d3
Matt Addicoat <maddicoat@users.noreply.github.com>
2023-09-18 09:53:32 +0100

    Create 2d_pes_plot.py

---
+++ 2d_pes_plot.py
@@ -0,0 +1,21 @@
+import os
+import numpy as np
+
+PES_file = ('Cl-CH3-OH_scan.txt')
+PES = np.genfromtxt(fname=PES_file, dtype='unicode')
+headers = PES[0]
+data = PES[1:]
+data = data.astype(float)
+
+clc = data[:,0]
+co = data[:,1]
+energy = data[:,2]
+
+import matplotlib.pyplot as plt
+from matplotlib import cm
+# plot
+fig, ax = plt.subplots()
+
+ax.scatter(clc, co, c=energy, vmin=min(energy), vmax=max(energy))
+
+plt.show()
